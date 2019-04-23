c1dab16043d706a2bd02d283945654775898ee18
jandreasyan <jandreasyan@gmail.com>
2019-04-23 23:16:37 +0200

    Update services.php

--- Site/services.php
+++ Site/services.php
@@ -1,3 +1,21 @@
 <?php
 include("entete.php");
 ?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+
+<div class ="block">
+  <div class="banner">
+      <img src="src/img/bg5.jpg" class="banner-image">
+      <div class="banner-content" style="">
+          <h1 class="subtitle">Découvrez nos offres!</h1>
+          <button class="button is-link" id="open_modal">Contactez-nous!</button>
+  </div>
+</div>
+
+<?php
+include("formulaire.php");
+?>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+<script src="src/js/main.js"></script>
