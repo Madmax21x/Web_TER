07e973ce222c358fb0d9088167bdeb781a4d32d0
jandreasyan <jandreasyan@gmail.com>
2019-04-23 22:19:46 +0200

    Create entete.php

---
+++ Site/entete.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <!-- head -->
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Site TER API</title>
+
+    <!-- css -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+    <link rel="stylesheet" href="src/css/main.css" type="text/css" media="all"/>
+    <!-- css -->
+
+    <!-- Font -->
+    <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville:700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville:700|Montserrat:700" rel="stylesheet">
+    <!-- Font -->
+
+    <!-- meta SEO -->
+    <meta name='description' content='Description de votre site'>
+    <meta name="keywords" content="webdesign, webdeveloppeur">
+    <!-- meta SEO -->
+
+
+  </head>
+  <!-- head -->
+
+  <!-- body -->
+  <body>
+    <!-- header -->
+    <div class="block">
+
+      <header class="header">
+        <a href="#" class="header-logo"> API - Twitter</a>
+        <nav class="header-menu">
+          <a href="index.php">Accueil</a>
+          <a href="services.php">Nos services</a>
+          <a href="travaux.php">Nos travaux</a>
+          <a href="press.php">Press</a>
+          <a href="contact.php">Contact</a>
+        </nav>
+      </header>
+    </div>
+  </body>
+    <!-- header -->
