ef4552b66fb61fbb90a8ffcb9093a71d10c4d1b3
jandreasyan <jandreasyan@gmail.com>
2019-04-23 22:19:25 +0200

    Create contact.php

---
+++ Site/contact.php
@@ -0,0 +1,42 @@
+<?php
+include("entete.php");
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<!-- Contact -->
+
+<h1 class="title is-1" style="color: #1abc9c; text-align: center; margin-top: 15%; ">Contactez-nous!</h1>
+
+<center>
+<div class="info" style="margin-left: 46%; font-size: 30px;">
+  <div class="field is-horizontal">
+    <a href="#" style="margin-right: 15px; "><i class="fab fa-twitter"></i></a>
+    <a href="#" style="margin-right: 15px;"><i class="fab fa-facebook-square"></i></a>
+    <a href="#" style="margin-right: 15px;"><i class="fab fa-instagram"></i></a>
+  </div>
+</div>
+</center>
+<br/>
+<br/>
+<br/>
+
+<div class="block">
+  <div class="container about">
+    <div class="columns">
+        <div class="column about-single-element">
+          <i class="fas fa-envelope-open-text icon"></i>
+          <p>ter.twitter@gmail.com</p>
+        </div>
+
+        <div class="column about-single-element">
+          <i class="fas fa-map-marker-alt icon"></i>
+          <p> Université de Bordeaux</p>
+          <p> 33 076 Bordeaux CEDEX</p>
+          </div>
+        <div class="column about-single-element">
+          <i class="fas fa-phone icon"></i>
+          <p>06 07 82 30 54</p>
+        </div>
+    </div>
+  </div>
+</div>
