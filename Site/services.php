cd287e63e8b48a215ce832117f3a7ddebc66e8e2
jandreasyan <jandreasyan@gmail.com>
2019-04-25 22:25:29 +0200

    Update services.php

--- Site/services.php
+++ Site/services.php
@@ -4,11 +4,11 @@ include("entete.php");
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
 
-<div class ="block">
-  <div class="banner">
-      <img src="src/img/bg5.jpg" class="banner-image">
-      <div class="banner-content" style="">
-          <h1 class="subtitle">Découvrez nos offres!</h1>
+<div class ="block" >
+  <div class="banner" >
+      <img src="src/img/bg12.jpg" class="banner-image" style="position: relative;">
+      <div class="banner-content-1" style="position: absolute; top: 25%; left: 5%;" >
+          <h1 class="subtitle" style="color:#FFFFFF; font-size: 60px;">Découvrez nos offres!</h1>
           <button class="button is-link" id="open_modal">Contactez-nous!</button>
   </div>
 </div>
@@ -19,3 +19,7 @@ include("formulaire.php");
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 <script src="src/js/main.js"></script>
+
+<?php
+include("pied.php");
+?>
