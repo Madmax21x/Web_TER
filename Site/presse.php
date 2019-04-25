afaa4acdd70cc45f62b7c09a32f77795cc760e63
jandreasyan <jandreasyan@gmail.com>
2019-04-25 22:25:22 +0200

    Update presse.php

--- Site/presse.php
+++ Site/presse.php
@@ -1,3 +1,28 @@
 <?php
 include("entete.php");
 ?>
+
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+
+<div class ="block" >
+  <div class="banner" >
+      <img src="src/img/bg15.jpg" class="banner-image" style="position: relative;">
+      <div class="banner-content-1" style="position: absolute; top: 24%; left: 5%;" >
+          <h1 class="subtitle" style="color:#FFFFFF; font-size: 60px;">Découvrez d'autres articles!</h1>
+          <button class="button is-link" id="open_modal">Contactez-nous!</button>
+  </div>
+</div>
+
+<?php
+include("formulaire.php");
+?>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+<script src="src/js/main.js"></script>
+
+
+<?php
+include("pied.php");
+?>
