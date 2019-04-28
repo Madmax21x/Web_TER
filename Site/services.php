ebdcad8bd000909b5317d7f4ac44058363f48036
jandreasyan <jandreasyan@gmail.com>
2019-04-28 15:54:08 +0200

    Update services.php

--- Site/services.php
+++ Site/services.php
@@ -24,27 +24,61 @@ include("formulaire.php");
   <br/>
   <div class="container about">
     <div class="columns">
-        <div class="column about-single-element-1">
+        <div class="column about-single-element-1" style="column-width: 270px;">
           <img src="src/img/reseau.jpg" class="banner-image" style="position: relative;">
         </div>
 
-        <div class="column about-single-element-1" style="text-align:justify;">
+        <div class="column about-single-element-1" style="text-align:justify; column-width: 500px;">
           <p>L’émergence et la multiplication des réseaux sociaux ont révolutionné notre
              manière de partager, dénoncer, nous exprimer, informer… Les individus partagent
-             de plus en plus leur quotidien, leurs centres d'intérêts, leurs émotions, leurs valeurs,
+             de plus en plus leurs quotidiens, leurs centres d'intérêts, leurs émotions, leurs valeurs,
              leurs habitudes de consommateurs.
            Le fait de partager permet de renforcer son "self-esteem" en suscitant des réactions parmi
          ses amis (likes, re-partages, retweets, commentaires) mais aussi de se sentir populaire ou influent.
          Pour avoir du succès, les contenus qu'on partage doivent créer de l'émotion, de l'admiration,
          de l'amusement chez les autres internautes. Il faut du contenu qui capte l'attention à travers le
-         visuel (images, vidéos, gifs..).
+         visuel (images, vidéos, gifs..). Les réseaux sociaux permettent aux individus de confronter
+         leurs opinions et de débattre autour d’un sujet d’actualité ou partager de l'information.
        </p>
+    </div>
+  </div>
+</div>
+
+<div class="block">
+  <br/>
+  <br/>
+  <div class="container about">
+    <div class="columns">
+        <div class="column about-single-element-1" style="text-align:justify;">
           <p>Les réseaux sociaux ont changé notre façon de consommer. Il devient indispensable
             pour les entreprises de changer leurs manières de communiquer. La communication se fait
             désormais dans les deux sens. Les publicités ne suffisent plus. La voix des consommateurs
             est plus qu'importante. Désormais les marques communiquent, les internautes interpellent
-             et il faut que les marques répondent en retour. </p>
-          </div>
+             et il faut que les marques répondent en retour. Les réseaux sociaux permettent de communiquer
+           avec une masse, un groupe, ou une communauté.
+         Ils présentent de nombreux avantages pour les entreprises qui franchissent le pas. </p>
+          C'est un très bon moyen pour rencontrer et se rapprocher de ses clients. Les réseaux sociaux soutiennent
+        les entreprises dans l'amélioration de l'image de leurs marques, l'accroissement de la notoriété
+      et de la visibilité. L'activité sur réseaux sociaux peut impacter positivement la
+    fidélisation des clients, créer de la récurrence d'achat et de la valeur pour l'entreprise. Les
+  réseaux sociaux ont un impact réel sur le comportement d'achat des internautes... </p>
+      </div>
+
+      <div class="column about-single-element-1" style="text-align:center; ">
+        <br/>
+        <p><span class="italique" style="font-style: italic; text-align:center;">"Un grand avantage des réseaux sociaux pour le
+          marketing est quand même que cela permet de parler directement aux consommateurs si besoin,
+          comme un espèce de chat public.
+          C'est ce que fait beaucoup Décathlon par exemple (la polémique autour de leur hijab
+          pour la course à pied), et ça permet une gestion de la clientèle beaucoup plus proche des
+          clients qu'on ne le faisait avant."</span></p>
+          <br/>
+          <p style="font-weight:bold;">Romain Damian, Data Scientist chez Liegey Muller Pons à Paris.</p>
+
+
+      </div>
+
+      </div>
     </div>
   </div>
 </div>
