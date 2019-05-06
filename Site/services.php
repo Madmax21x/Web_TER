26d3484edd5c779bea7ffda2f72e8a021a69f853
jandreasyan <jandreasyan@gmail.com>
2019-05-06 16:19:53 +0200

    Update services.php

--- Site/services.php
+++ Site/services.php
@@ -80,155 +80,11 @@ include("formulaire.php");
   </div>
 </div>
 
-
-<div class="block">
-  <br/>
-  <br/>
-  <h2 class="subtitle heading-site-1" style= "margin-left: 10%; font-size: 30px;">L'e-réputation à travers les réseaux sociaux</h2>
-  <br/>
-  <div class="container about">
-    <div class="columns">
-
-        <div class="column about-single-element-1" style="text-align:justify;">
-          <p>L'e-réputation est la perception qu'ont les internautes d'une entreprise,
-            de sa marque ou de ses employés. C'est une représentation que les internautes
-            se constituent en fonction des flux d'informations qu'ils rencontrent sur internet.
-            Désormais, la réputation d'une entreprise peut se mesurer en nombre d’avis, de
-            recommandations ou de citations spontanément émis sur la marque dans les conversations en ligne.
-            On s'intéresse surtout aux informations trouvées sur les réseaux sociaux.
-            Maîtriser son e-réputation sur les réseaux sociaux est devenu une nécessité absolue
-            pour les entreprises et les marques.
-
-            Lorsqu'un internaute tape le nom d'une marque ou d'une entreprise sur internet, il tombe
-            facilement sur des résultats issus des réseaux sociaux. Ces résultats représentent l'image
-            de la marque et la réputation de l'entreprise. Les réseaux sociaux permettent aux consommateurs
-            d'être mieux informés sur les produits et les services qu'ils recherchent. Ils peuvent facilement
-            vérifier ce qu'ont laissé comme avis (ou commentaires) d'autres utilisateurs et en prendre
-            compte avant leur achat. Or ce que les internautes découvrent sur une marque, ils peuvent le
-            dénoncer et tout peut se savoir. Le plus gros risque que peut craindre une entreprise sur les
-            réseaux sociaux c'est le bad bazz.
-       </p>
-    </div>
-
-    <div class="column about-single-element-1">
-      <br/>
-      <img src="src/img/logos.jpg" class="banner-image" style="position: relative; width: 350px; margin-left: 20%;">
-    </div>
-
-  </div>
-</div>
-
-<div class="block">
-  <br/>
-  <br/>
-  <div class="container about">
-    <div class="columns">
-
-        <div class="column about-single-element-1" style="text-align:center;">
-          <p><span class="italique" style="font-style: italic;">"La priorité du matin quand tu arrives au travail,
-            c’est de répondre aux avis de TripAdvisor. On est à 10 avis par jour sur les gros jours. On a 4500 avis sur TripAdvisor,
-            8000 sur Google...Il faut avoir une bonne réputation sur TripAdvisor
-            pour ne pas prendre du retard en ligne. Ça a une énorme importance. Je réponds aux avis, puis tous les mois j’envoie
-            les 5 meilleurs avis et les 5 pires avis du mois, qui font un peu office des avis du mois, sachant qu’il y a d’autres
-            enquêtes de satisfaction à côté.</span></p>
-            <p><span class="italique" style="font-style: italic; text-align:center;">
-            Les avis Tripadvisor servent vraiment de baromètre. On a eu une mauvaise note ce mois-ci qu’est-ce qu’il s’est passé?
-            C’est vraiment la différence entre une structure récente et une structure comme le Louvre qui a une histoire que nous
-            ne l'avons pas. On est obligé de s’améliorer grace à ces avis et c’est pour ça qu’on n'a pas de très bonnes notes. Un
-            des reproches qu’on nous fait c’est que quand il y a du monde, l’expérience est horrible ici et les gros WE quand on
-            sait qu’il va y avoir du monde, on sait que le lundi on va s’en prendre sur TripAdvisor, donc on anticipe, on met un
-            petit message « soyez sympa, venez plus tôt, faites un coup de fil … ».  On anticipe les besoins de nos clients grace
-             aux avis."
-
-        </span></p>
-          <br/>
-          <p style="font-weight:bold;">Maxime Chabaud, Community and content Manager à la Cité du Vin.</p>
-
-
-    </div>
-
-    <div class="column about-single-element-1" style="text-align:center;">
-      <p> <span class="italique" style="font-style: italic; text-align:center;">
-        "Ce qui est important, c'est qu'il y a 2 types de structures: celles qui existaient avant les réseaux
-        sociaux, comme le Louvre, et des structures comme nous. Des structures comme le Louvre
-        ont des avis bien supérieurs à la nôtre parce qu’ils ont amélioré leurs musées avant
-        les avis, alors que nous c’est grace aux avis sur TripAdvisor qu’on s’améliore."</span></p>
-        <br/>
-        <p style="font-weight:bold;">Timothée Binet, Développeur SI & Multimédia à la Cité du Vin.</p>
-  </div>
-</div>
-
-<div class="block">
-  <br/>
-  <br/>
-  <h2 class="subtitle heading-site-1" style= "margin-left: 5%; font-size: 30px;">L'e-réputation dans la stratégie des entreprises</h2>
-  <br/>
-  <div class="container about">
-    <div class="columns">
-        <div class="column about-single-element-1">
-          <img src="src/img/chess.jpg" class="banner-image" style="position: relative; width:500px; margin-left:5%;">
-        </div>
-
-        <div class="column about-single-element-1" style="text-align:justify;">
-          <p>L'e-réputation d'une entreprise peut être anticipée, modifiée et maîtrisée
-            comme n'importe quel autre type de publicité. La gestion de l'e-réputation
-            doit être prise en compte dans la stratégie d'une organisation.
-            Les entreprises doivent donc veiller constamment sur leur e-réputation, pour
-            pouvoir en tirer du bénéfice.
-            Une entreprise doit construire elle-même son e-réputation et contrôler soi-même
-            ses informations. Elle doit par ailleurs la protéger pour éviter qu'elle soit
-            entachée par des avis peu élogieux. Il faut essayer d'être toujours informé en
-            temps réel d'un changement de situation et savoir vite réagir. Un seul avis
-            négatif suffit pour changer la perception qu'on les internautes sur la marque de
-            l'entreprise. Lorsque les internautes cherchent de l'information sur une entreprise,
-            c'est pour trouver plus de crédibilité en la marque et un mauvais avis peut faire
-            perdre des clients potentiels à l'entreprise. </p>
-
-    </div>
-  </div>
-</div>
-
-<div class="block">
-  <br/>
-  <br/>
-  <div class="container about">
-    <div class="columns">
-        <div class="column about-single-element-1" style="text-align:justify;">
-            <p>Les internautes sont libres de partager leurs avis et leurs opinions sur les
-              réseaux. C'est ce qui rend le contôle plus difficile pour une entreprise. Lorsque
-              ces avis deviennent très nombreux, il devient difficile pour une entreprise de gérer
-              son e-réputation sans avoir recours à des techniques d'automatisation. En effet,
-            afin d'étudier son e-réputation l'entreprise doit d'abord être en capacité de
-          récupérer sur les réseaux toutes les données qui la concernent pour pouvoir les analyser.</p>
-          <p>Les réseaux sociaux sont un formidable laboratoire pour tester la perception réelle
-            des messages de communication de la marque. Ils peuvent être le déclencheur d’une
-            vraie prise de conscience, celle de la nécessité de réaliser un plan marketing autrement
-             : plus humblement, plus à l’écoute du consommateur, en mode collaboratif.</p>
-
-    </div>
-
-    <div class="column about-single-element-1" style="text-align:center;">
-    </span></p>
-    <p><span class="italique" style="font-style: italic; text-align:center;">
-      "Les 20 % du budget marketing sont consacrés au suivi des réseaux sociaux. Je pense que le budget est conséquent.
-      Mon poste a été créé la deuxième année, donc il y a eu une prise de conscience de l’importance des réseaux sociaux."
-
-
-  </span></p>
-    <br/>
-    <p style="font-weight:bold;">Maxime Chabaud, Community and content Manager à la Cité du Vin.</p>
-
-
-
-</div>
-  </div>
-</div>
-
 <div class="block">
   <br/>
   <br/>
   <br/>
-  <h2 class="subtitle heading-site-1" style= "margin-left: 5%; font-size: 30px;">Le comportement et le choix du consommateur</h2>
+  <h2 class="subtitle heading-site-1" style= "margin-left: 10%; font-size: 30px;">Le comportement et le choix du consommateur</h2>
   <br/>
   <div class="container about">
     <div class="columns">
@@ -297,6 +153,207 @@ include("formulaire.php");
       </div>
     </div>
 
+
+
+<div class="block">
+  <br/>
+  <br/>
+  <h2 class="subtitle heading-site-1" style= "margin-left: 10%; font-size: 30px;">L'e-réputation à travers les réseaux sociaux</h2>
+  <br/>
+  <div class="container about">
+    <div class="columns">
+
+      <div class="column about-single-element-1">
+        <br/>
+        <img src="src/img/logos.jpg" class="banner-image" style="position: relative; width: 350px; margin-left: 20%;">
+      </div>
+
+        <div class="column about-single-element-1" style="text-align:justify;">
+          <p>L'e-réputation est la perception qu'ont les internautes d'une entreprise,
+            de sa marque ou de ses employés. C'est une représentation que les internautes
+            se constituent en fonction des flux d'informations qu'ils rencontrent sur internet.
+            Désormais, la réputation d'une entreprise peut se mesurer en nombre d’avis, de
+            recommandations ou de citations spontanément émis sur la marque dans les conversations en ligne.
+            On s'intéresse surtout aux informations trouvées sur les réseaux sociaux.
+            Maîtriser son e-réputation sur les réseaux sociaux est devenu une nécessité absolue
+            pour les entreprises et les marques.
+
+            Lorsqu'un internaute tape le nom d'une marque ou d'une entreprise sur internet, il tombe
+            facilement sur des résultats issus des réseaux sociaux. Ces résultats représentent l'image
+            de la marque et la réputation de l'entreprise. Les réseaux sociaux permettent aux consommateurs
+            d'être mieux informés sur les produits et les services qu'ils recherchent. Ils peuvent facilement
+            vérifier ce qu'ont laissé comme avis (ou commentaires) d'autres utilisateurs et en prendre
+            compte avant leur achat. Or ce que les internautes découvrent sur une marque, ils peuvent le
+            dénoncer et tout peut se savoir. Le plus gros risque que peut craindre une entreprise sur les
+            réseaux sociaux c'est le bad bazz.
+       </p>
+    </div>
+
+
+
+  </div>
+</div>
+
+<div class="block">
+  <br/>
+  <br/>
+  <div class="container about">
+    <div class="columns">
+
+        <div class="column about-single-element-1" style="text-align:center;">
+          <p><span class="italique" style="font-style: italic;">"La priorité du matin quand tu arrives au travail,
+            c’est de répondre aux avis de TripAdvisor. On est à 10 avis par jour sur les gros jours. On a 4500 avis sur TripAdvisor,
+            8000 sur Google...Il faut avoir une bonne réputation sur TripAdvisor
+            pour ne pas prendre du retard en ligne. Ça a une énorme importance. Je réponds aux avis, puis tous les mois j’envoie
+            les 5 meilleurs avis et les 5 pires avis du mois, qui font un peu office des avis du mois, sachant qu’il y a d’autres
+            enquêtes de satisfaction à côté.</span></p>
+            <p><span class="italique" style="font-style: italic; text-align:center;">
+            Les avis Tripadvisor servent vraiment de baromètre. On a eu une mauvaise note ce mois-ci qu’est-ce qu’il s’est passé?
+            C’est vraiment la différence entre une structure récente et une structure comme le Louvre qui a une histoire que nous
+            ne l'avons pas. On est obligé de s’améliorer grace à ces avis et c’est pour ça qu’on n'a pas de très bonnes notes. Un
+            des reproches qu’on nous fait c’est que quand il y a du monde, l’expérience est horrible ici et les gros WE quand on
+            sait qu’il va y avoir du monde, on sait que le lundi on va s’en prendre sur TripAdvisor, donc on anticipe, on met un
+            petit message « soyez sympa, venez plus tôt, faites un coup de fil … ».  On anticipe les besoins de nos clients grace
+             aux avis."
+
+        </span></p>
+          <br/>
+          <p style="font-weight:bold;">Maxime Chabaud, Community and content Manager à la Cité du Vin.</p>
+
+
+    </div>
+
+    <div class="column about-single-element-1" style="text-align:center;">
+      <p> <span class="italique" style="font-style: italic; text-align:center;">
+        "Ce qui est important, c'est qu'il y a 2 types de structures: celles qui existaient avant les réseaux
+        sociaux, comme le Louvre, et des structures comme nous. Des structures comme le Louvre
+        ont des avis bien supérieurs à la nôtre parce qu’ils ont amélioré leurs musées avant
+        les avis, alors que nous c’est grace aux avis sur TripAdvisor qu’on s’améliore."</span></p>
+        <br/>
+        <p style="font-weight:bold;">Timothée Binet, Développeur SI & Multimédia à la Cité du Vin.</p>
+  </div>
+</div>
+
+<div class="block">
+  <br/>
+  <br/>
+  <h2 class="subtitle heading-site-1" style= "margin-left: 5%; font-size: 30px;">L'e-réputation dans la stratégie des entreprises</h2>
+  <br/>
+  <div class="container about">
+    <div class="columns">
+
+
+        <div class="column about-single-element-1" style="text-align:justify;">
+          <p>L'e-réputation d'une entreprise peut être anticipée, modifiée et maîtrisée
+            comme n'importe quel autre type de publicité. La gestion de l'e-réputation
+            doit être prise en compte dans la stratégie d'une organisation.
+            Les entreprises doivent donc veiller constamment sur leur e-réputation, pour
+            pouvoir en tirer du bénéfice.
+            Une entreprise doit construire elle-même son e-réputation et contrôler soi-même
+            ses informations. Elle doit par ailleurs la protéger pour éviter qu'elle soit
+            entachée par des avis peu élogieux. Il faut essayer d'être toujours informé en
+            temps réel d'un changement de situation et savoir vite réagir. Un seul avis
+            négatif suffit pour changer la perception qu'on les internautes sur la marque de
+            l'entreprise. Lorsque les internautes cherchent de l'information sur une entreprise,
+            c'est pour trouver plus de crédibilité en la marque et un mauvais avis peut faire
+            perdre des clients potentiels à l'entreprise. </p>
+
+    </div>
+
+    <div class="column about-single-element-1">
+      <img src="src/img/chess.jpg" class="banner-image" style="position: relative; width:500px; margin-left:5%;">
+    </div>
+  </div>
+</div>
+
+<div class="block">
+  <br/>
+  <br/>
+  <div class="container about">
+    <div class="columns">
+        <div class="column about-single-element-1" style="text-align:justify;">
+            <p>Les internautes sont libres de partager leurs avis et leurs opinions sur les
+              réseaux. C'est ce qui rend le contôle plus difficile pour une entreprise. Lorsque
+              ces avis deviennent très nombreux, il devient difficile pour une entreprise de gérer
+              son e-réputation sans avoir recours à des techniques d'automatisation. En effet,
+            afin d'étudier son e-réputation l'entreprise doit d'abord être en capacité de
+          récupérer sur les réseaux toutes les données qui la concernent pour pouvoir les analyser.</p>
+          <p>Les réseaux sociaux sont un formidable laboratoire pour tester la perception réelle
+            des messages de communication de la marque. Ils peuvent être le déclencheur d’une
+            vraie prise de conscience, celle de la nécessité de réaliser un plan marketing autrement
+             : plus humblement, plus à l’écoute du consommateur, en mode collaboratif.</p>
+
+    </div>
+
+    <div class="column about-single-element-1">
+    <p>Certaines entreprises sont plus conscientes que d'autres des opportunités que leurs réservent les réseaux
+      sociaux. On distingue alors deux types d'entreprises: les data conscious et les moins avertis. Les data conscious
+      sont ceux qui sont conscients de la richesse des données clients exploitables en recherche d’insights
+      sur les réseaux sociaux et qui les utilisent dans leur communication, dans leur développement produit
+      et fluidification de la relation client. Les moins avertis sont ceux en totale découverte. Ils sont présents
+      sur les réseaux sociaux et les considèrent comme un canal à part sans forcément penser que des
+      opportunités business s’y trouvent.
+    <br/>
+    </span></p>
+    <p><span class="italique" style="font-style: italic; text-align:center;">
+      "Les 20 % du budget marketing sont consacrés au suivi des réseaux sociaux. Je pense que le budget est conséquent.
+      Mon poste a été créé la deuxième année, donc il y a eu une prise de conscience de l’importance des réseaux sociaux."
+
+
+  </span></p>
+    <br/>
+    <p style="font-weight:bold;">Maxime Chabaud, Community and content Manager à la Cité du Vin.</p>
+
+
+
+
+</div>
+  </div>
+</div>
+
+
+
+    <div class="block">
+      <br/>
+      <br/>
+      <h2 class="subtitle heading-site-1" style= "margin-left: 5%; font-size: 30px;">Le Social Data et les réseaux sociaux</h2>
+      <br/>
+      <div class="container about">
+        <div class="columns">
+            <div class="column about-single-element-1">
+              <img src="src/img/big_data.jpg" class="banner-image" style="position: relative; width:500px; margin-left:5%;">
+
+            </div>
+
+            <div class="column about-single-element-1" style="text-align:justify;">
+              <p>L'analyse des données générées par les réseaux sociaux, aussi appelée
+                Social Media Analytics, peut être très utile pour les entreprises de toutes les industries.
+                En effet, ces données permettent de connaître le ressenti des clients à l’égard d’un
+                produit, d’un service, ou d’une campagne marketing, mais aussi de vérifier le niveau de
+                satisfaction des clients, ou encore de surveiller la concurrence.
+              </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="block">
+      <br/>
+      <br/>
+      <div class="container about">
+        <div class="columns">
+            <div class="column about-single-element-1" style="text-align:justify;">
+              </div>
+
+          <div class="column about-single-element-1" style="text-align:center; ">
+            <br/>
+            <p><span class="italique" style="font-style: italic;"></span></p>
+              <br/>
+              <p style="font-weight:bold;">Romain Damian, Data Scientist chez Liegey Muller Pons à Paris.</p>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
 
 
