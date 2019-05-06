c4c9ffb33d55e1f665ffae494ffdd8bd9ae23fbd
Maxence <max21maz07@gmail.com>
2019-05-06 17:27:52 +0200

    Update presse.php

--- Site/presse.php
+++ Site/presse.php
@@ -18,6 +18,36 @@ include("entete.php");
 <?php
 include("formulaire.php");
 ?>
+<div class="container">
+  <br/>
+  <br/>
+  <div class="columns">
+  <div class="column is-two-thirds notification is-primary has-text-centered">RESSOURCES</div>
+  <div class="column"></div>
+  <div class="column"></div>
+  </div>
+  <div class="columns">
+    <div class="column is-two-thirds notification is-light">Quel avenir pour le Social Listening ?</div>
+    <div class="column notification is-primary has-text-centered"><a href="https://siecledigital.fr/2019/01/22/quel-avenir-pour-le-social-listening/">Lien</div>
+    <div class="column">By Jonathan Charbit</a></div>
+  </div>
+  <div class="columns">
+    <div class="column is-two-thirds notification is-light">is-three-quarters</div>
+    <div class="column notification is-primary"> Auto</div>
+    <div class="column">Auto</div>
+  </div>
+  <div class="columns">
+    <div class="column is-two-thirds notification is-light">is-four-fifths</div>
+    <div class="column notification is-primary">Auto</div>
+    <div class="column">Auto</div>
+  </div>
+  <div class="columns">
+    <div class="column is-two-thirds notification is-light">is-four-fifths</div>
+    <div class="column notification is-primary">Auto</div>
+    <div class="column">Auto</div>
+  </div>
+</div>
+
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 <script src="src/js/main.js"></script>
