a8a1f003f8aa4f6229c9e6fe0f5be1d8b6205aad
Maxence <max21maz07@gmail.com>
2019-05-08 18:48:06 +0200

    Update presse.php

--- Site/presse.php
+++ Site/presse.php
@@ -3,180 +3,169 @@ include("entete.php");
 ?>
 
 
-<!DOCTYPE html>
-<html lang="fr" dir="ltr">
+    <div class ="block" >
+      <div class="banner" >
+          <img src="src/img/bg15.jpg" class="banner-image" style="position: relative;">
+          <div class="banner-content-1" style="position: absolute; top: 24%; left: 5%;" >
+              <h1 class="subtitle" style="color:#FFFFFF; font-size: 60px;">Découvrez d'autres articles!</h1>
+      </div>
+    </div>
 
-<div class ="block" >
-  <div class="banner" >
-      <img src="src/img/bg15.jpg" class="banner-image" style="position: relative;">
-      <div class="banner-content-1" style="position: absolute; top: 24%; left: 5%;" >
-          <h1 class="subtitle" style="color:#FFFFFF; font-size: 60px;">Découvrez d'autres articles!</h1>
-          <button class="button is-link" id="open_modal">Contactez-nous!</button>
-  </div>
-</div>
 
-<?php
-include("formulaire.php");
-?>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <div class="container">
+      <div class="columns">
+        <div class="column">
 
-<br/>
-<br/>
-<br/>
-<br/>
-<div class="container">
-  <div class="columns">
-    <div class="column">
-
-      <div class="card notification is-light">
-        <div class="card-content">
-          <p class="title">
-            Réseaux sociaux et comportement d'achat sur Internet.
-          </p>
-          <p class="subtitle">
-            By Upmybiz
-          </p>
+          <div class="card notification is-light">
+            <div class="card-content">
+              <p class="title">
+                Réseaux sociaux et comportement d'achat sur Internet.
+              </p>
+              <p class="subtitle">
+                By Upmybiz
+              </p>
+            </div>
+            <footer class="card-footer">
+              <p class="card-footer-item">
+                <span>
+                  Voir le lien <a href="https://www.upmybiz.com/reseaux-sociaux-et-impact-comportement-achat/">ici</a>
+                </span>
+              </p>
+            </footer>
+          </div>
         </div>
-        <footer class="card-footer">
-          <p class="card-footer-item">
-            <span>
-              Voir le lien <a href="https://www.upmybiz.com/reseaux-sociaux-et-impact-comportement-achat/">ici</a>
-            </span>
-          </p>
-        </footer>
-      </div>
-    </div>
 
-    <div class="column">
-      <div class="card notification is-grey">
-        <div class="card-content">
-          <p class="title">
-            The Importance of Online Reputation Management for Businesses.</p>
-          <p class="subtitle">
-            By Jenna Seter, Senior Business Analyst & Marketer, Clutch
-          </p>
+        <div class="column">
+          <div class="card notification is-grey">
+            <div class="card-content">
+              <p class="title">
+                The Importance of Online Reputation Management for Businesses.</p>
+              <p class="subtitle">
+                By Jenna Seter, Senior Business Analyst & Marketer, Clutch
+              </p>
+            </div>
+            <footer class="card-footer">
+              <p class="card-footer-item">
+                <span>
+                  Voir le lien <a href="https://clutch.co/pr-firms/resources/importance-online-reputation-management-businesses">ici</a>
+                </span>
+              </p>
+            </footer>
+          </div>
         </div>
-        <footer class="card-footer">
-          <p class="card-footer-item">
-            <span>
-              Voir le lien <a href="https://clutch.co/pr-firms/resources/importance-online-reputation-management-businesses">ici</a>
-            </span>
-          </p>
-        </footer>
-      </div>
-    </div>
 
-    <div class="column">
-      <div class="card notification is-grey-dark">
-        <div class="card-content">
-          <p class="title">
-            Les risques de réputation liés aux réseaux sociaux</p>
-          <p class="subtitle">
-            By Bolero
-          </p>
+        <div class="column">
+          <div class="card notification is-grey-dark">
+            <div class="card-content">
+              <p class="title">
+                Les risques de réputation liés aux réseaux sociaux</p>
+              <p class="subtitle">
+                By Bolero
+              </p>
+            </div>
+            <footer class="card-footer">
+              <p class="card-footer-item">
+                <span>
+                  Voir le lien <a href="https://www.bolero.fr/article-chaine-expert/les-risques-de-reputation-lies-aux-reseaux-sociaux/">ici</a>
+                </span>
+              </p>
+            </footer>
+          </div>
         </div>
-        <footer class="card-footer">
-          <p class="card-footer-item">
-            <span>
-              Voir le lien <a href="https://www.bolero.fr/article-chaine-expert/les-risques-de-reputation-lies-aux-reseaux-sociaux/">ici</a>
-            </span>
-          </p>
-        </footer>
+
+
+    </div>
+
+    <div class="columns">
+      <div class="column">
+
+        <div class="card notification is-dark">
+          <div class="card-content">
+            <p class="title">
+              Réseaux sociaux et comportement d'achat sur Internet.
+            </p>
+            <p class="subtitle">
+              By Upmybiz
+            </p>
+          </div>
+          <footer class="card-footer">
+            <p class="card-footer-item">
+              <span>
+                Voir le lien <a href="https://www.upmybiz.com/reseaux-sociaux-et-impact-comportement-achat/">ici</a>
+              </span>
+            </p>
+          </footer>
+        </div>
+      </div>
+
+      <div class="column">
+        <div class="card notification is-grey-light">
+          <div class="card-content">
+            <p class="title">
+              The Importance of Online Reputation Management for Businesses.</p>
+            <p class="subtitle">
+              By Jenna Seter, Senior Business Analyst & Marketer, Clutch
+            </p>
+          </div>
+          <footer class="card-footer">
+            <p class="card-footer-item">
+              <span>
+                Voir le lien <a href="https://clutch.co/pr-firms/resources/importance-online-reputation-management-businesses">ici</a>
+              </span>
+            </p>
+          </footer>
+        </div>
+      </div>
+
+      <div class="column">
+        <div class="card notification is-primary">
+          <div class="card-content">
+            <p class="title">
+              Les risques de réputation liés aux réseaux sociaux</p>
+            <p class="subtitle">
+              By Bolero
+            </p>
+          </div>
+          <footer class="card-footer">
+            <p class="card-footer-item">
+              <span>
+                Voir le lien <a href="https://www.bolero.fr/article-chaine-expert/les-risques-de-reputation-lies-aux-reseaux-sociaux/">ici</a>
+              </span>
+            </p>
+          </footer>
+        </div>
       </div>
     </div>
 
 
-</div>
-
-<div class="columns">
-  <div class="column">
-
-    <div class="card notification is-dark">
-      <div class="card-content">
-        <p class="title">
-          Réseaux sociaux et comportement d'achat sur Internet.
-        </p>
-        <p class="subtitle">
-          By Upmybiz
-        </p>
-      </div>
-      <footer class="card-footer">
-        <p class="card-footer-item">
-          <span>
-            Voir le lien <a href="https://www.upmybiz.com/reseaux-sociaux-et-impact-comportement-achat/">ici</a>
-          </span>
-        </p>
-      </footer>
     </div>
-  </div>
 
-  <div class="column">
-    <div class="card notification is-grey-light">
-      <div class="card-content">
-        <p class="title">
-          The Importance of Online Reputation Management for Businesses.</p>
-        <p class="subtitle">
-          By Jenna Seter, Senior Business Analyst & Marketer, Clutch
-        </p>
+    <h1 class="title is-1" style="color: #1abc9c; text-align: center; margin-top: 7%; ">Références</h1>
+
+      <div class="container">
+      <br/>
+      <br/>
+      <div class="columns">
+        <div class="column is-two-thirds notification is-light" style="margin-right:1%;">Réseaux sociaux et comportement d'achat sur Internet.</div>
+        <div class="column notification is-light" style="text-align: center; margin-right:1%;">By Upmybiz</div>
+        <div class="column notification is-light" style="text-align: center;"><a href="https://www.upmybiz.com/reseaux-sociaux-et-impact-comportement-achat/">Lien</a></div>
       </div>
-      <footer class="card-footer">
-        <p class="card-footer-item">
-          <span>
-            Voir le lien <a href="https://clutch.co/pr-firms/resources/importance-online-reputation-management-businesses">ici</a>
-          </span>
-        </p>
-      </footer>
-    </div>
-  </div>
-
-  <div class="column">
-    <div class="card notification is-primary">
-      <div class="card-content">
-        <p class="title">
-          Les risques de réputation liés aux réseaux sociaux</p>
-        <p class="subtitle">
-          By Bolero
-        </p>
+      <div class="columns">
+        <div class="column is-two-thirds notification is-light" style="margin-right:1%;">Réseaux sociaux et comportement d'achat sur Internet.</div>
+        <div class="column notification is-light" style="text-align: center; margin-right:1%;">By Upmybiz</div>
+        <div class="column notification is-light" style="text-align: center;"><a href="https://www.upmybiz.com/reseaux-sociaux-et-impact-comportement-achat/">Lien</a></div>
       </div>
-      <footer class="card-footer">
-        <p class="card-footer-item">
-          <span>
-            Voir le lien <a href="https://www.bolero.fr/article-chaine-expert/les-risques-de-reputation-lies-aux-reseaux-sociaux/">ici</a>
-          </span>
-        </p>
-      </footer>
+      <div class="columns">
+        <div class="column is-two-thirds notification is-light" style="margin-right:1%;">Réseaux sociaux et comportement d'achat sur Internet.</div>
+        <div class="column notification is-light" style="text-align: center; margin-right:1%;">By Upmybiz</div>
+        <div class="column notification is-light" style="text-align: center;"><a href="https://www.upmybiz.com/reseaux-sociaux-et-impact-comportement-achat/">Lien</a></div>
+      </div>
+
     </div>
-  </div>
-</div>
-
-
-</div>
-
-<h1 class="title is-1" style="color: #1abc9c; text-align: center; margin-top: 7%; ">Références</h1>
-
-  <div class="container">
-  <br/>
-  <br/>
-  <div class="columns">
-    <div class="column is-two-thirds notification is-light" style="margin-right:1%;">Réseaux sociaux et comportement d'achat sur Internet.</div>
-    <div class="column notification is-light" style="text-align: center; margin-right:1%;">By Upmybiz</div>
-    <div class="column notification is-light" style="text-align: center;"><a href="https://www.upmybiz.com/reseaux-sociaux-et-impact-comportement-achat/">Lien</a></div>
-  </div>
-  <div class="columns">
-    <div class="column is-two-thirds notification is-light" style="margin-right:1%;">Réseaux sociaux et comportement d'achat sur Internet.</div>
-    <div class="column notification is-light" style="text-align: center; margin-right:1%;">By Upmybiz</div>
-    <div class="column notification is-light" style="text-align: center;"><a href="https://www.upmybiz.com/reseaux-sociaux-et-impact-comportement-achat/">Lien</a></div>
-  </div>
-  <div class="columns">
-    <div class="column is-two-thirds notification is-light" style="margin-right:1%;">Réseaux sociaux et comportement d'achat sur Internet.</div>
-    <div class="column notification is-light" style="text-align: center; margin-right:1%;">By Upmybiz</div>
-    <div class="column notification is-light" style="text-align: center;"><a href="https://www.upmybiz.com/reseaux-sociaux-et-impact-comportement-achat/">Lien</a></div>
-  </div>
-
-</div>
-
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-<script src="src/js/main.js"></script>
 
 
 <?php
