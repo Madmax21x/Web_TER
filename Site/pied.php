56d60f1cbeb0dbe374bef3459cd3d87033a6c964
Maxence <max21maz07@gmail.com>
2019-05-08 13:32:33 +0200

    Update pied.php

--- Site/pied.php
+++ Site/pied.php
@@ -2,22 +2,23 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css">
-<script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
-<link rel="stylesheet" href="src/css/main.css" type="text/css" media="all"/>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css">
+  <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+  <link rel="stylesheet" href="src/css/main.css" type="text/css" media="all"/>
 
-<center>
-  <footer style="bottom: 0; margin-bottom:0; position: relative;left: 50; right:50; margin-top: 30px;">
-  <div class="footer-informations" style="margin-bottom: 30px;">
-    <p> Université de Bordeaux</p>
-    <p> 33 076 Bordeaux CEDEX</p>
-    <div class="info" style="margin-left: 46%; font-size: 30px;">
-      <div class="field is-horizontal">
-        <a href="#" style="margin-right: 15px; "><i class="fab fa-twitter"></i></a>
-        <a href="#" style="margin-right: 15px;"><i class="fab fa-facebook-square"></i></a>
-        <a href="#" style="margin-right: 15px;"><i class="fab fa-instagram"></i></a>
+  <center>
+    <footer style="bottom: 0; margin-bottom:0; position: relative;left: 50; right:50; margin-top: 30px;">
+    <div class="footer-informations" style="margin-bottom: 30px;">
+      <p> Université de Bordeaux</p>
+      <p> 33 076 Bordeaux CEDEX</p>
+      <div class="info" style="margin-left: 46%; font-size: 30px;">
+        <div class="field is-horizontal">
+          <a href="#" style="margin-right: 15px; "><i class="fab fa-twitter"></i></a>
+          <a href="#" style="margin-right: 15px;"><i class="fab fa-facebook-square"></i></a>
+          <a href="#" style="margin-right: 15px;"><i class="fab fa-instagram"></i></a>
+        </div>
       </div>
     </div>
-  </div>
-</footer>
-</center>
+  </footer>
+  </center>
+</html>
