5a75b335464e184b492b11beaeeceddd1f8d6dea
jandreasyan <jandreasyan@gmail.com>
2019-05-08 22:29:32 +0200

    Update presse.php

--- Site/presse.php
+++ Site/presse.php
@@ -19,28 +19,27 @@ include("entete.php");
     <div class="container">
       <div class="columns">
         <div class="column">
-
-          <div class="card notification is-light">
+          <div class="card notification">
             <div class="card-content">
               <p class="title">
-                Réseaux sociaux et comportement d'achat sur Internet.
-              </p>
+                Les limites de la rationalité limitée? Un essai de réflexion en sciences de gestion</p>
               <p class="subtitle">
-                By Upmybiz
+                By Véronique Chanut, Nathalie Guibert, Jacques Rojot et Pierre-Louis Dubois
               </p>
             </div>
             <footer class="card-footer">
               <p class="card-footer-item">
                 <span>
-                  Voir le lien <a href="https://www.upmybiz.com/reseaux-sociaux-et-impact-comportement-achat/">ici</a>
+                  Voir le lien <a href="https://www.cairn.info/revue-management-et-avenir-2011-8-page-97.html#">ici</a>
                 </span>
               </p>
             </footer>
           </div>
         </div>
 
+
         <div class="column">
-          <div class="card notification is-grey">
+          <div class="card notification is-light">
             <div class="card-content">
               <p class="title">
                 The Importance of Online Reputation Management for Businesses.</p>
@@ -77,13 +76,12 @@ include("entete.php");
           </div>
         </div>
 
-
     </div>
 
     <div class="columns">
-      <div class="column">
 
-        <div class="card notification is-dark">
+      <div class="column">
+        <div class="card notification is-primary">
           <div class="card-content">
             <p class="title">
               Réseaux sociaux et comportement d'achat sur Internet.
@@ -103,18 +101,146 @@ include("entete.php");
       </div>
 
       <div class="column">
-        <div class="card notification is-grey-light">
+
+        <div class="card">
           <div class="card-content">
             <p class="title">
-              The Importance of Online Reputation Management for Businesses.</p>
+              Consommateur et numérique : quel impact sur les comportements d’achat ?
+            </p>
             <p class="subtitle">
-              By Jenna Seter, Senior Business Analyst & Marketer, Clutch
+              By Zencom
             </p>
           </div>
           <footer class="card-footer">
             <p class="card-footer-item">
               <span>
-                Voir le lien <a href="https://clutch.co/pr-firms/resources/importance-online-reputation-management-businesses">ici</a>
+                Voir le lien <a href="http://www.zencommunication.fr/2017/09/21/consommateur-numerique-impact-comportements-dachat/">ici</a>
+              </span>
+            </p>
+          </footer>
+        </div>
+      </div>
+
+      <div class="column">
+        <div class="card notification is-dark">
+          <div class="card-content">
+            <p class="title">
+              Big Social Data : que faire des données générées sur les réseaux sociaux ?</p>
+            <p class="subtitle">
+              By Mélanie Harouel
+            </p>
+          </div>
+          <footer class="card-footer">
+            <p class="card-footer-item">
+              <span>
+                Voir le lien <a href="https://www.powertrafic.fr/big-social-data-donnees-reseaux-sociaux/">ici</a>
+              </span>
+            </p>
+          </footer>
+        </div>
+      </div>
+
+    </div>
+      <div class="columns">
+
+        <div class="column">
+          <div class="card notification is-dark">
+            <div class="card-content">
+              <p class="title">
+                Qu'est-ce que la Social Data?
+              </p>
+              <p class="subtitle">
+                By NATHAN BLAISON
+              </p>
+            </div>
+            <footer class="card-footer">
+              <p class="card-footer-item">
+                <span>
+                  Voir le lien <a href="https://wearesocial.com/fr/blog/2018/01/social-data">ici</a>
+                </span>
+              </p>
+            </footer>
+          </div>
+        </div>
+
+        <div class="column">
+
+          <div class="card notification is-light">
+            <div class="card-content">
+              <p class="title">
+                C’est quoi une API ?
+              </p>
+              <p class="subtitle">
+                By Rémi Mercier
+              </p>
+            </div>
+            <footer class="card-footer">
+              <p class="card-footer-item">
+                <span>
+                  Voir le lien <a href="https://medium.com/@mercier_remi/c-est-quoi-une-api-f37ae350cb9">ici</a>
+                </span>
+              </p>
+            </footer>
+          </div>
+        </div>
+
+        <div class="column">
+          <div class="card">
+            <div class="card-content">
+              <p class="title">Les APIs sont les nouveaux contrats du monde numérique. Un entretien avec Mehdi Medjaoui.</p>
+              <p class="subtitle">
+                By Ouishare Team
+              </p>
+            </div>
+            <footer class="card-footer">
+              <p class="card-footer-item">
+                <span>
+                  Voir le lien <a href="https://www.ouishare.net/article/les-apis-sont-les-nouveaux-contrats-du-monde-numerique?locale=en_us">ici</a>
+                </span>
+              </p>
+            </footer>
+          </div>
+        </div>
+
+    </div>
+
+    <div class="columns">
+
+      <div class="column">
+        <div class="card notification is-light">
+          <div class="card-content">
+            <p class="title">
+              Les APIs pour les nuls
+            </p>
+            <p class="subtitle">
+              By Claus T. Jensen
+            </p>
+          </div>
+          <footer class="card-footer">
+            <p class="card-footer-item">
+              <span>
+                Voir le lien <a href="https://www.ibm.com/cloud-computing/API_pour_les_nuls_WSM14025FRFR_3_of_5.pdf">ici</a>
+              </span>
+            </p>
+          </footer>
+        </div>
+      </div>
+
+      <div class="column">
+
+        <div class="card">
+          <div class="card-content">
+            <p class="title">
+              Twitter et ses API : le réseau social serre encore la vis
+            </p>
+            <p class="subtitle">
+              By Louis Adam
+            </p>
+          </div>
+          <footer class="card-footer">
+            <p class="card-footer-item">
+              <span>
+                Voir le lien <a href="https://www.zdnet.fr/actualites/twitter-et-ses-api-le-reseau-social-serre-encore-la-vis-39868348.htm">ici</a>
               </span>
             </p>
           </footer>
@@ -124,22 +250,22 @@ include("entete.php");
       <div class="column">
         <div class="card notification is-primary">
           <div class="card-content">
-            <p class="title">
-              Les risques de réputation liés aux réseaux sociaux</p>
+            <p class="title">Exploiter les données de réseaux sociaux en 5 étapes</p>
             <p class="subtitle">
-              By Bolero
+              By Ecosystème Digital et Data, Social networks
             </p>
           </div>
           <footer class="card-footer">
             <p class="card-footer-item">
               <span>
-                Voir le lien <a href="https://www.bolero.fr/article-chaine-expert/les-risques-de-reputation-lies-aux-reseaux-sociaux/">ici</a>
+                Voir le lien <a href="https://www.kernix.com/blog/exploiter-les-donnees-de-reseaux-sociaux-en-5-etapes_p22">ici</a>
               </span>
             </p>
           </footer>
         </div>
       </div>
-    </div>
+
+  </div>
 
 
     </div>
