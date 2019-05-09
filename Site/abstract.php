dffc977aba22b1df7d0618c44303d122ecdc25b9
Maxence <max21maz07@gmail.com>
2019-05-09 16:45:54 +0200

    Create abstract.php

---
+++ Site/abstract.php
@@ -0,0 +1,17 @@
+<?php
+include("entete.php");
+?>
+    <script src="src/js/main.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <div class ="block" >
+      <div class="banner" >
+          <img src="src/img/bg10.jpg" class="banner-image" style="position: relative;">
+          <div class="banner-content-1" style="position: absolute; top: 25%; left: 5%;" >
+              <h1 class="subtitle" style="color:#FFFFFF; font-size: 60px;">Abstract</h1>
+      </div>
+    </div>
+
+
+<?php
+include("pied.php");
+?>
