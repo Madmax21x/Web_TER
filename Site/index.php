7b92a623c062cb43b384a9cfd655a8a03ece6807
Maxence <max21maz07@gmail.com>
2019-05-10 10:13:41 +0200

    Update index.php

--- Site/index.php
+++ Site/index.php
@@ -25,15 +25,15 @@ include("entete.php");
             <div class="tile">
               <div class="tile is-parent is-vertical">
                 <article class="tile is-child notification ">
-                  <p class="title"><a href="services.php#reseaux" style="text-decoration:none">Pourquoi s'intéresser aux réseaux sociaux?</a></p></p>
-                  <p class="subtitle"><a href="services.php#reseaux" style="text-decoration:none">- Les réseaux sociaux et les consommateurs</a></p>
-                  <p class="subtitle"><a href="services.php#api" style="text-decoration:none">- Qu'est-ce qu'une API?</a></p>
+                  <p class="title"><a href="services11.php#reseaux" style="text-decoration:none">Pourquoi s'intéresser aux réseaux sociaux?</a></p></p>
+                  <p class="subtitle"><a href="services1.php#reseaux" style="text-decoration:none">- Les réseaux sociaux et les consommateurs</a></p>
+                  <p class="subtitle"><a href="services1.php#api" style="text-decoration:none">- Qu'est-ce qu'une API?</a></p>
                 </article>
                 <article class="tile is-child notification is-warning">
-                  <p class="title"><a href="services.php#services" style="text-decoration:none">Quels sont les services que nous vous proposons?</a></p></p>
+                  <p class="title"><a href="services1.php#services1" style="text-decoration:none">Quels sont les services que nous vous proposons?</a></p></p>
 
-                  <p class="subtitle"><a href="services.php#e_reputation" style="text-decoration:none">- L'e-réputation à travers les réseaux sociaux</a></p>
-                  <p class="subtitle"><a href="services.php#strategy" style="text-decoration:none">- L'e-réputation dans la stratégie des entreprises</a></p>
+                  <p class="subtitle"><a href="services1.php#e_reputation" style="text-decoration:none">- L'e-réputation à travers les réseaux sociaux</a></p>
+                  <p class="subtitle"><a href="services1.php#strategy" style="text-decoration:none">- L'e-réputation dans la stratégie des entreprises</a></p>
                 </article>
               </div>
               <div class="tile is-parent">
