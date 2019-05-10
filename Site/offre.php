c6737896a66fd34532bbc86c7e0ae7d9c397b63c
Maxence <max21maz07@gmail.com>
2019-05-10 10:59:32 +0200

    Create offre.php

---
+++ Site/offre.php
@@ -0,0 +1,38 @@
+<div class="container">
+  <div>
+    <br/>
+    <h1 id="services" class="title is-1" style="color:#718093; text-align: center; margin-top: 7%; "><i class="fas fa-question-circle"></i> Quels sont les services que nous vous proposons?</h1>
+  </div>
+</div>
+
+<div class="block">
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <div class="container about">
+    <div class="columns">
+      <div class="column about-single-element-1" style="text-align:center; ">
+        <h2 class="subtitle heading-site-1" style= "margin-left: 5%; font-size: 30px;">Pie2Eat fait l'étude de votre e-réputation</h2>
+        <div>
+          <p> Pie2Eat vous aide à soigner votre e-réputation en identifiant les
+            commentaires et les tweets négatifs sur votre marque. Elle vous aide à
+            savoir comment votre marque est-elle perçue sur les réseaux et en
+            particulier sur Twitter. </p>
+          <p>Pie2Eat identifie tous les tweets où votre nom a été mentionné.
+            Elle analyse ces tweets pour vous, pour connaître leurs origines,
+            la date de publication, le nombre de likes, le nombre de retweets,
+            mais aussi le sentiment: est-ce que le tweet est positif pour votre
+            marque, négatif ou neutre. </p>
+      </div>
+      </div>
+
+      <div class="column about-single-element-1" style="text-align:justify; ">
+        <div>
+          <img src="src/img/network.jpg" class="is-rounded" style="position: relative; width:500px; height:300px;">
+      </div>
+      </div>
+
+    </div>
+  </div>
+</div>
