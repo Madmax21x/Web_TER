def7360d8faa88853b478fbe7303fe58604fe61a
jandreasyan <jandreasyan@gmail.com>
2019-05-10 11:14:09 +0200

    Update offre.php

--- Site/offre.php
+++ Site/offre.php
@@ -1,21 +1,19 @@
 <?php
 include("entete.php");
 ?>
-    <script src="src/js/main.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-    <div class ="block" >
-      <div class="banner" >
-        <img src="src/img/bg12.jpg" class="banner-image" style="position: relative;">
+  <script src="src/js/main.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <div class ="block" >
+    <div class="banner" >
+        <img src="src/img/bg10.jpg" class="banner-image" style="position: relative;">
         <div class="banner-content-1" style="position: absolute; top: 25%; left: 5%;" >
-            <h1 class="subtitle" style="color:#FFFFFF; font-size: 60px;">Découvrez nos solutions!</h1>
-        </div>
-      </div>
+            <h1 class="subtitle" style="color:#FFFFFF; font-size: 60px;">Découvrez nos offres!</h1></div>
     </div>
+  </div>
 
 
 <div class="container is-fluid">
   <div class="notification">
-
     <div class="block">
     <br/>
     <div class="container about">
@@ -40,7 +38,13 @@ include("entete.php");
             <img src="src/img/network.jpg" class="is-rounded" style="position: relative; width:500px; height:300px;">
         </div>
         </div>
-
       </div>
     </div>
   </div>
+</div>
+</div>
+
+
+  <?php
+  include("pied.php");
+  ?>
