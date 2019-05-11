5fec5f9ce894c63116a70062d2f2bff5d54beafa
jandreasyan <jandreasyan@gmail.com>
2019-05-11 14:22:26 +0200

    Update abstract.php

--- Site/abstract.php
+++ Site/abstract.php
@@ -17,21 +17,21 @@ include("entete.php");
     <br/>
     <br/>
     <div style="margin-left:15%; margin-right:15%; margin-bottom:10%; text-align:justify;">
-  <p>The main goal of a company is to create value and make a profit. The growth of
-    the Internet and social media requires a new way of managing public perception and so the
-    reputation of a company. Indeed, the online reputation management helps drive public opinion
+  <p>The main goal of a company is to create value and make profit. The growth of
+    the Internet and social media requires to manage public perception differently and therefore a new
+    way to manage companies' reputation. Indeed, the online reputation management helps driving public opinion
     about a business, its products and services.</p>
     <br/>
-  <p>All social networks have APIs (Application Programming Interface) which give access to lots of data about users (consumers).</p>
+  <p>All social networks have an API (Application Programming Interface) which gives access to lots of data about its users (consumers).</p>
   <p>Through our research, we would like to show how social network APIs can be used as a new management tool for businesses.
-    To reach our goal we chose Twitter’s API. We wrote a Python program which uses Twitter’s API to access data on twitter.
-    The program allows us to make requests, get wanted informations and analyze it in a fraction of a second. For instance,
-    it is possible to show on a graph the evolution of likes or retweets on the profile of a company and so study its reputation.</p>
+    To illustrate our idea we chose Twitter’s API. We wrote a Python program which uses Twitter’s API to access its users' datas on twitter.
+    The program allows us to make various requests, get pieces of information and analyze it in a fraction of a second. For instance,
+    we have the ability to show on a graph the evolution of likes or retweets on the profile of a company and therefore study its reputation.</p>
   <br/>
- <p>As expected, the analysis of the data we are able to recover on social networks can be a good indicator of the online
-   reputation of a company. Indeed, we can see how the company is perceived by the others, but also study the behaviour
-   of its clients and the consumers in general. It is also possible to get information about the new trends.
-   A company can make an appropriate offer, be attractive and fully satisfy its clients.</p>
+ <p>To analyse social medias' data can be a good indicator of a company's online reputation. It gives us the opportunity to see how 
+   the company is perceived by others, but also study the behaviour
+   of its clients and consumers in general. It is also possible to get information about the new trends.
+   This way, a company can make an appropriate offer, be attractive and fully satisfy its clients.</p>
 
  </div>
   </div>
